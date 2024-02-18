efab624042cfa8bf483a6a8233b9135466a7a34c
HsiaoIJen <s123unny@gmail.com>
2024-02-18 12:27:05 -0800

    [Scanner] Add scan string and number literal

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,7 +1,9 @@
 use std::str::Chars;
+use std::str::FromStr;
 use std::fs::File;
 use std::io::{BufRead,BufReader};
 use std::iter::Peekable;
+use std::process;
 
 pub struct Scanner {
 	file_path: String,
@@ -13,7 +15,7 @@ pub struct Scanner {
 impl Scanner {
 	pub fn new(file_path: &String) -> Self {
 		let f = BufReader::new(File::open(file_path).expect("Couldn't read file"));
-		let s = Self { file_path: file_path.clone(), f: f, line: 1, tokens: Vec::new()};
+		let s = Self { file_path: file_path.clone(), f: f, line: 0, tokens: Vec::new()};
 		s
 	}
 	pub fn scan_tokens(&mut self) {
@@ -29,15 +31,43 @@ impl Scanner {
 	}
 
 	fn chars_at_end(&mut self, chars: &mut Peekable<Chars<'_>>) -> bool {
-		chars.peek().is_none()
+		chars.peek().is_none() || chars.peek() == Some(&'\n')
 	}
 
 	fn scan_token(&mut self, chars: &mut Peekable<Chars<'_>>) {
-		let c = chars.next();
+		let Some(c) = chars.next() else {
+			panic!("Application error: Can't scan token");
+		};
 		match c {
-			Some('(') => self.add_token(TokenType::LeftParen),
-			Some(')') => self.add_token(TokenType::RightParen),
-			_ => println!("ignore now"),
+			'(' => self.add_token(TokenType::LeftParen),
+			')' => self.add_token(TokenType::RightParen),
+			'{' => self.add_token(TokenType::LeftBrace),
+			'}' => self.add_token(TokenType::RightBrace),
+			'-' => self.add_token(TokenType::Minus),
+			'+' => self.add_token(TokenType::Plus),
+			'/' => self.add_token(TokenType::Slash),
+			'*' => self.add_token(TokenType::Star),
+			';' => self.add_token(TokenType::Semicolon),
+			'!' => {
+				let token = if self.expect(chars, '=') { TokenType::NotEqual } else { TokenType::Not };
+				self.add_token(token);
+			},
+			'=' => {
+				let token = if self.expect(chars, '=') { TokenType::EqualEqual } else { TokenType::Equal };
+				self.add_token(token);
+			},
+			'>' => {
+				let token = if self.expect(chars, '=') { TokenType::GreaterEqual } else { TokenType::Greater };
+				self.add_token(token);
+			},
+			'<' => {
+				let token = if self.expect(chars, '=') { TokenType::LessEqual } else { TokenType::Less };
+				self.add_token(token);
+			},
+			' ' | '\t' | '\r' => {},
+			'"' => self.scan_string(chars),
+			n @ '0'..='9' => self.scan_number(chars, n),
+			_ => println!("todo {:?}", c),
 		}
 	}
 
@@ -45,37 +75,116 @@ impl Scanner {
 		self.tokens.push(Token { token: t, line: self.line });
 	}
 
-	pub fn print_token(&self) {
-		for token in self.tokens.iter() {
-			match token.token {
-				TokenType::LeftParen => println!("("),
-				TokenType::RightParen => println!(")"),
-				_ => println!("."),
+	fn expect(&mut self, chars: &mut Peekable<Chars<'_>>, expected: char) -> bool {
+		if self.chars_at_end(chars) || chars.peek() != Some(&expected) {
+			return false;
+		}
+		chars.next();
+		true
+	}
+
+	fn scan_string(&mut self, chars: &mut Peekable<Chars<'_>>) {
+		let mut string = String::new();
+		while let Some(c) = chars.next() {
+			if c == '"' {
+				break;
+			} else {
+				string.push(c)
 			}
 		}
+		if self.chars_at_end(chars) {
+			self.error("Unterminated string")
+		} else {
+			self.add_token(TokenType::String(string));
+		}
+	}
+
+	fn scan_number(&mut self, chars: &mut Peekable<Chars<'_>>, c: char) {
+		let mut string = String::new();
+		let mut base = 10;
+		if c == '0' && chars.peek() == Some(&'x') {
+			chars.next();
+			base = 16;
+		} else {
+			string.push(c);
+		}
+		let mut is_float = false;
+		while let Some(c) = chars.peek() {
+			match c {
+				c @ '0'..='9'| c @ 'a'..='f'| c @ 'A'..='F' => string.push(*c),
+				c @ '.' => {
+					if is_float || base == 16 {
+						self.error("Unrecognized number literal");
+					} else {
+						is_float = true;
+						string.push(*c);
+					}
+				},
+				_ => break,
+			}
+			chars.next();
+		}
+		if is_float {
+			let Ok(number) = f64::from_str(&string) else {
+				self.error("Unrecognized number literal");
+				return;
+			};
+			self.add_token(TokenType::Float(number));
+		} else {
+			let Ok(number) = u64::from_str_radix(&string, base) else {
+				self.error("Unrecognized number literal");
+				return;
+			};
+			self.add_token(TokenType::Integer(number));
+		}
+	}
+
+	fn error(&self, m: &str) {
+		println!("{}:{} Error: {}", self.file_path, self.line, m);
+		process::exit(1);
+	}
+
+	pub fn print_token(&self) {
+		let mut line = 0;
+		for token in self.tokens.iter() {
+			if token.line > line {
+				line = token.line;
+				print!("\n{}: ", line);
+			}
+			match &token.token {
+				TokenType::LeftParen => print!("("),
+				TokenType::RightParen => print!(")"),
+				TokenType::String(s) => print!("String<{}>", s),
+				TokenType::Integer(n) => print!("Integer<{}>", n),
+				TokenType::Float(n) => print!("Float<{}>", n),
+				_ => print!("."),
+			}
+			print!(" ");
+		}
+		println!("");
 	}
 }
 
 enum TokenType {
   // Single-character tokens.
-  LeftParen, RightParen, //LEFT_BRACE, RIGHT_BRACE,
-  //MINUS, PLUS, SLASH, STAR, SEMICOLON,
+  LeftParen, RightParen, LeftBrace, RightBrace,
+  Minus, Plus, Slash, Star, Semicolon,
 
   // One or two character tokens.
-  //NOT, NOT_EQUAL,
-  //EQUAL, EQUAL_EQUAL,
-  //GREATER, GREATER_EQUAL,
-  //LESS, LESS_EQUAL,
+  Not, NotEqual,
+  Equal, EqualEqual,
+  Greater, GreaterEqual,
+  Less, LessEqual,
 
   // Literals.
-  //STRING(String), INTEGER(i64), FLOAT(f64), TRUE, FALSE,
+  String(String), Integer(u64), Float(f64), True, False,
 
-	//IDENTIFIER(String),
+	Identifier(String),
 
   // Keywords.
-  //DEF, FOR, IF, ELSE, WHILE, NIL,
-  //RETURN, VAR,
-	//AND, OR,
+  Def, For, If, Else, While, Nil,
+  Return, Var,
+	I32, I64, F32, F64, Bool,
 }
 
 struct Token {
